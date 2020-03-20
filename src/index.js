742e9eb42083dbdcccea700df775e1e6884df8f1
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-20 18:28:29 +0900

    before add redux

--- src/index.js
+++ src/index.js
@@ -3,3 +3,32 @@
 // import App from './App';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+const add = document.querySelector('#add')
+const minus = document.querySelector('#minus')
+const result = document.querySelector('#result')
+
+let count = 0
+
+
+const updateText = () => {
+  result.innerText = count
+}
+
+
+updateText()
+
+const handleAdd = () => {
+  count += 1
+  updateText()
+}
+
+const handleMinus = () => {
+  count -= 1
+  updateText()
+}
+
+add.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)

21a4ab98c6b2ddf86dc9ddb73e0747b68432afb8
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-20 19:06:26 +0900

    after use redux

--- src/index.js
+++ src/index.js
@@ -4,31 +4,39 @@
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-
+import { createStore } from 'redux'
 
 const add = document.querySelector('#add')
 const minus = document.querySelector('#minus')
 const result = document.querySelector('#result')
 
-let count = 0
+const Add = 'Add'
+const Minus = 'Minus'
 
+// reducer : reducer modifies stored data
+// reducer가 return 하는 값이 데이터에 저장됨
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case Add:
+      return state + 1
+    case Minus:
+      return state - 1
+    default:
+      return state
+  }
 
-const updateText = () => {
-  result.innerText = count
 }
 
+const store = createStore(reducer)
 
-updateText()
-
-const handleAdd = () => {
-  count += 1
-  updateText()
+// subscribe : 변화된 state를 감자하게 해준다
+const handleChange = () => {
+  result.innerText = store.getState()
 }
 
-const handleMinus = () => {
-  count -= 1
-  updateText()
-}
+store.subscribe(handleChange)
 
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+
+store.dispatch({type: 'init'})
+add.addEventListener('click', () => store.dispatch({type: Add}))
+minus.addEventListener('click', () => store.dispatch({type: Minus}))
